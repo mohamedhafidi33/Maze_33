0d69ef9e0050cd078752079b5e83a9c4043f4234
Mohamed Hafidi <72892818+mohamedhafidi33@users.noreply.github.com>
2021-09-28 23:02:42 +0100

    Add files via upload

---
+++ SDL_Test/Coordinate.h
@@ -0,0 +1,32 @@
+/*
+	La structure de coordonnées est une structure de données qui contient des informations de coordonnées.
+    Il est utilisé par les Rooms, les MazeObjects et les classes dérivées de MazeObjects
+*/
+
+#pragma once
+struct Coordinate
+{
+public:
+	int xPos;
+	int yPos;
+	Coordinate(int x, int y);
+	Coordinate();
+	~Coordinate();
+
+	bool operator ==(const Coordinate &other) const
+	{
+		if (xPos == other.xPos && yPos == other.yPos)
+			return true;
+		else
+			return false;
+	}
+
+	bool operator !=(const Coordinate &other) const
+	{
+		if (xPos != other.xPos || yPos != other.yPos)
+			return true;
+		else
+			return false;
+	}
+};
+
